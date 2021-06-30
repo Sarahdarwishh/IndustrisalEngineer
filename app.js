3031ba5d8673dbcb60fe80fe86ae3119d73bce07
SarahDarwish <>
2021-06-30 19:11:18 +0000

    add  dynamic components

---
+++ app.js
@@ -0,0 +1,11 @@
+var Major = prompt("What is your university major?")
+
+alert(" welcome to our successful " + Major)
+  
+prompt("Did you know that there is a specialty called industrial engineering before?(Yes/ No)")
+
+
+
+
+
+
