c2c2aefcc84255185bc21316d819f547e6805369
SarahDarwish <>
2021-06-30 19:28:38 +0000

    add dynamic changes

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var Major = prompt("What is your university major?")
 
 alert(" welcome to our successful " + Major)
-  
+
 prompt("Did you know that there is a specialty called industrial engineering before?(Yes/ No)")
 
 
